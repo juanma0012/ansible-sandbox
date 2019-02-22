a5660e7fa757a43723d464a7910645ac29c07a36
Juan Sanchez <juanma0012@gmail.com>
2019-02-21 21:25:58 -0500

    working load balancer (haproxy), webservers (nodejs) and database (mongodb)

--- templates/server.js
+++ templates/server.js
@@ -2,11 +2,6 @@ const express = require("express");
 const path = require('path');
 const app = new express();
 
-//app.use(express.static(__dirname + '/public'));
-app.get("/", function(req, resp) {
-  resp.sendFile(path.resolve(__dirname + '/public', 'index.html'));
-});
-app.get("/public/app.js", function(req, resp) {
-  resp.sendFile(path.resolve(__dirname + '/public/', 'app.js'));
-});
+app.use(express.static(__dirname + '/public'));
+
 app.listen("80");
